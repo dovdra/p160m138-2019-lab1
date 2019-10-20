e537a3d2f91cedf51916e8523769e627cda97ace
dovdra <ddrazdovaite@gmail.com>
2019-10-20 10:46:26 +0000

    updated hello

--- hello.py
+++ hello.py
@@ -2,8 +2,17 @@
 
 import sys
 
-def greet(greeted_name):
-	print "Hello, ",greeted_name,"!"
+def greet(name):
+	shout_count = 1
+	if int(sys.argv[2]) != 1:
+		shout_count = int(sys.argv[2])	
+
+	sys.stdout.write("Hello, ")
+	sys.stdout.write(name )
+
+	for i in range(shout_count):
+		sys.stdout.write("!")
+	print ""
 	return;
 
 greet(str(sys.argv[1]))
